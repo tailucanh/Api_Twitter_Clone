4625d81b3372e28c9adcd8092bb2dff59b47bb89
Lục Anh Tài <93418649+tailucanh@users.noreply.github.com>
2023-07-20 13:27:06 +0700

    code update

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 require("dotenv").config();
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+
+const express = require("express");
 var mongoose = require("mongoose");
 const PORT = process.env.PORT || 8000;
 const usersRouter = require("./routes/routers_users");
@@ -12,16 +9,8 @@ const commentsRouter = require("./routes/routers_comments");
 const likesRouter = require("./routes/routers_likes");
 
 var app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hbs");
-
-app.use(logger("dev"));
+mongoose.set("strictQuery", false);
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
