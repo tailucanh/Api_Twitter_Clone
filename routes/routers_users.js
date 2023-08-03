dda3d84b4750585476776b0b6ebe620efab5ab07
Lục Anh Tài <93418649+tailucanh@users.noreply.github.com>
2023-08-04 00:29:14 +0700

    api message update 10

--- routes/routers_users.js
+++ routes/routers_users.js
@@ -90,7 +90,7 @@ router.post("/login", async (req, res) => {
 });
 
 router.post("/loginWithId", async (req, res) => {
-  const userId = req.params.id;
+  const userId = req.body.userId;
   const userLogin = await UserModel.findOne({ _id: userId });
 
   if (!userLogin) {
